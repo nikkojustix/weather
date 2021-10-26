3825c8dae4ec9ab50890ef1ee8b64cb079632b07
nikkojustix <nickolaykotov@gmail.com>
2021-10-26 16:54:49 +0300

    added main component(plain)

--- src/components/Header/City.tsx
+++ src/components/Header/City.tsx
@@ -4,8 +4,6 @@ interface Props {
   cityName: string
 }
 
-
-
 const City = (props: Props) => {
   const getCity = (e: any) => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + e.target.textContent + '&units=metric&appid=71d4cc3b96e2310c2b89b64f623f285b')
